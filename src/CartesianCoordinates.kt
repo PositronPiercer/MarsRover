addb85da384b16f5cfba848e87675220f03f3516
Shaikh Mamun Hoque <2017ucs0111@iitjammu.ac.in>
2021-09-20 12:21:21 +0530

    added test

--- src/CartesianCoordinates.kt
+++ src/CartesianCoordinates.kt
@@ -4,37 +4,57 @@ import java.lang.IllegalStateException
 class CartesianCoordinates(
     private val coordinates: List<Int>
 ) : Coordinates() {
+    private val dimension = coordinates.size
     fun reportCartesian2d(coordinatesReporter: CoordinatesReporter){
         coordinatesReporter.reportCartesian2d(coordinates[0], coordinates[1])
     }
-
-    override fun atDelta(distance: Int, direction: Orientation) : CartesianCoordinates {
+    private fun getDelta(distance: Int, direction: Orientation) : CartesianCoordinates{
         return when(direction){
             Orientation.N -> CartesianCoordinates(
                 coordinates = listOf(
-                    coordinates[0],
-                    coordinates[1] + distance
-                ) + coordinates.drop(2)
+                    0,
+                    distance
+                ) + List(dimension - 2){0}
             )
             Orientation.S -> CartesianCoordinates(
                 coordinates = listOf(
-                    coordinates[0],
-                    coordinates[1] - distance
-                ) + coordinates.drop(2)
+                    0,
+                    -distance
+                ) + List(dimension - 2){0}
             )
             Orientation.E -> CartesianCoordinates(
                 coordinates = listOf(
-                    coordinates[0] + distance,
-                    coordinates[1]
-                ) + coordinates.drop(2)
+                    distance,
+                    0
+                ) + List(dimension - 2){0}
             )
             Orientation.W -> CartesianCoordinates(
                 coordinates = listOf(
-                    coordinates[0] - distance,
-                    coordinates[1]
-                ) + coordinates.drop(2)
+                    -distance,
+                    0
+                ) + List(dimension - 2){0}
             )
         }
+    }
+
+    override fun atDelta(distance: Int, direction: Orientation) : CartesianCoordinates {
+        val delta = getDelta(distance, direction)
+        return this.add(delta)
+    }
+
+    fun add(delta: CartesianCoordinates): CartesianCoordinates {
+        val newCoordinatesList = mutableListOf<Int>()
+        lateinit var deltaCoordinates : List<Int>
+        val coordinatesReporter = object : CoordinatesReporter{
+            override fun reportCartesian(coordinates: List<Int>) {
+                deltaCoordinates = coordinates
+            }
+        }
+        delta.report(coordinatesReporter)
+        for (index : Int in 0 until dimension){
+            newCoordinatesList.add(deltaCoordinates[index] + coordinates[index])
+        }
+        return CartesianCoordinates(newCoordinatesList)
     }
 
     override fun report(coordinatesReporter: CoordinatesReporter) {
--- src/interfaces/CoordinatesReporter.kt
+++ src/interfaces/CoordinatesReporter.kt
@@ -1,7 +1,7 @@
 package interfaces
 
 interface CoordinatesReporter {
-    fun reportCartesian2d(x : Int, y : Int)
+    fun reportCartesian2d(x : Int, y : Int){}
     fun reportCartesian(coordinates : List<Int>){
         if (coordinates.size == 2){
             reportCartesian2d(coordinates[0], coordinates[1])
